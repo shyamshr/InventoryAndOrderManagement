56140c5b77b862de30cff2fd33371a1daa2618cd
shyamshr <146857616+shyamshr@users.noreply.github.com>
2025-01-04 17:55:42 +0530

    Add files via upload

---
+++ SeedData.sql
@@ -0,0 +1,6 @@
+---Seed Data for products
+INSERT INTO `flobizassignment`.`product` (`id`, `is_deleted`, `name`, `price`, `quantity_in_stock`, `sku`) VALUES ('1', 0, 'HPLaptop', '60000', '20', 'HP123');
+INSERT INTO `flobizassignment`.`product` (`id`, `is_deleted`, `name`, `price`, `quantity_in_stock`, `sku`) VALUES ('2', 0, 'Iphone 16', '80000', '30', 'Iphone123');
+INSERT INTO `flobizassignment`.`product` (`id`, `is_deleted`, `name`, `price`, `quantity_in_stock`, `sku`) VALUES ('3', 0, 'Iphone 15', '60000', '50', 'Iphone1234');
+INSERT INTO `flobizassignment`.`product` (`id`, `is_deleted`, `name`, `price`, `quantity_in_stock`, `sku`) VALUES ('4', 0, 'Samsung m35', '20000', '100','Sam123');
+INSERT INTO `flobizassignment`.`product` (`id`, `is_deleted`, `name`, `price`, `quantity_in_stock`, `sku`) VALUES ('5', 0, 'Samsung m20', '10000', '10','Sam1234');
